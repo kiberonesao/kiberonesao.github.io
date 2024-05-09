43a63ea443f482117958c4bf2e1ae3c4e75efb48
kiberonesao <159615356+kiberonesao@users.noreply.github.com>
2024-05-09 19:01:54 +0300

    f3-upd

---
+++ f3/kirill/scripts.js
@@ -0,0 +1,17 @@
+let a=[];
+a.push(prompt())
+a.push(prompt())
+a.push(prompt())
+alert(a)
+//программа получает на выход три строки,
+//1) получить от челов ека число
+let ch = []
+let buffer = parseInt(prompt)();
+// 2) пров ерить, является ли число четным
+
+if (buffer%2 == 0){
+    ch.push(butter*3);
+}
+else{
+ch.push( (butter+10/3))                                                    
+}
